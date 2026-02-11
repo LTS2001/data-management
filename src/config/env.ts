5f87d2e18759481372a3a997b6c20eb288c123fc
mxy <maxingyuan@ama-auto.com>
2026-02-11 14:15:18 +0800

    chore: 环境变量

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,4 @@
-// @ts-expect-error Vite 特有的环境变量注入，TypeScript 默认不识别
-const env = import.meta.env;
+const env = process.env;
 
 export const UMI_APP_ENV = env?.UMI_APP_ENV || 'development';
 
@@ -7,5 +6,5 @@ export const isProduction = UMI_APP_ENV === 'production';
 
 export const IS_MICRO_APP = window.__POWERED_BY_QIANKUN__;
 export const APP_API_URL = isProduction
-  ? 'https://cartea.icartea.com'
+  ? 'https://management.icartea.com'
   : 'https://cartea-dev.xiaofeilun.cn';
